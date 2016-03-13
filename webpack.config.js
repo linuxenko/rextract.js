6d834c37801373a32d3081ec7e4cc619733482ba
linuxenko <linuxenko@yahoo.com>
2016-03-13 09:38:52 +0200

    0.2.1

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ var webpack = require('webpack');
 var dir_js = path.resolve(__dirname, 'lib');
 var dir_build = path.resolve(__dirname, 'dist');
 
-module.exports = {
+module.exports = [{
     entry: path.resolve(dir_js, 'rextract.js'),
     output: {
         path: dir_build,
@@ -37,4 +37,30 @@ module.exports = {
     stats: {
         colors: true
     }
-}
+}, {
+entry: path.resolve(dir_js, 'rextract.js'),
+output: {
+    path: dir_build,
+    filename: 'rextract-module.js',
+    library : 'Rextract',
+    libraryTarget : 'commonjs2'
+},
+module: {
+    loaders: [
+      {
+        loader: "babel-loader",
+        include: [
+          path.resolve(__dirname, "lib"),
+        ],
+        test: /\.js?$/,
+        query: {
+          presets: ['es2015'],
+        }
+      }
+    ]
+},
+plugins: [
+    new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    new webpack.NoErrorsPlugin()
+]
+}];
